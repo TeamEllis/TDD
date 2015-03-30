6f4652012dc1926eb6955ff48e0fb4b852a2d123
Enki <enki.tomalak@laposte.net>
2015-03-30 14:03:45 +0200

    test au vert

--- tp-tdd/src/test/java/iut/tdd/Convert.java
+++ tp-tdd/src/test/java/iut/tdd/Convert.java
@@ -2,9 +2,19 @@ package iut.tdd;
 
 public class Convert {
 	public static String num2text(String input) {
-		return null;
+		if(input.equals("0")){
+			return "zéro";
+		}
+		else{
+			return null;
+		}
 	}
 	public static String text2num(String input) {
-		return null;
+		if(input.equals("zéro")){
+			return "0";
+		}
+		else{
+			return null;
+		}
 	}
 }
